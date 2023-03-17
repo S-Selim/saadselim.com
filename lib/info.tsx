1f08632e9a820e4cb70dc88036fe467238ed7915
Saad Selim <saadselim6@gmail.com>
2023-03-17 14:07:09 -0400

    update

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,34 +1,18 @@
-import me from "../app/placeholder.jpg";
+import me from '../app/placeholder.jpg';
 
-export const name = "Saad Selim";
+export const name = 'Your Name';
 export const avatar = me;
-
-export const profession =
-  "I'm a full-stack developer specializing in React and Node.js.";
-
-export const bio = "When I'm not coding, I'm probably playing video games.";
-
-export const socialMediaLinks = [
-  {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/saadselim/",
-  },
-  {
-    name: "GitHub",
-    url: "https://github.com/S-Selim",
-  },
-  {
-    name: "Twitter",
-    url: "https://twitter.com/ss6942",
-  },
-];
-
-export const skills = [
-  "JavaScript",
-  "React",
-  "Node.js",
-  "HTML/CSS",
-  "Git",
-  "MongoDB",
-  "Express",
-];
+export const about = () => {
+  return (
+    <>
+      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
+    </>
+  );
+};
+export const bio = () => {
+  return (
+    <>
+      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+    </>
+  );
+};

decbc6ca66ce2bd2ad738ed5a5904314c28b1e29
Saad Selim <saadselim6@gmail.com>
2023-03-17 12:24:09 -0400

    Udate info.tsx

--- lib/info.tsx
+++ lib/info.tsx
@@ -3,18 +3,8 @@ import me from "../app/placeholder.jpg";
 export const name = "Saad Selim";
 export const avatar = me;
 export const about = () => {
-  return (
-    <>
-      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as
-      a Software Engineer.
-    </>
-  );
+  return <>Hey, I'm a developer.</>;
 };
 export const bio = () => {
-  return (
-    <>
-      When I'm not coding, I'm probably playing video games or watching anime. I
-      also love to travel and explore new places.
-    </>
-  );
+  return <>When I'm not coding, I'm probably playing video games.</>;
 };

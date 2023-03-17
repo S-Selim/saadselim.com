234b47f60c8252a56792c81a541ea091c00c6722
Saad Selim <saadselim6@gmail.com>
2023-03-17 13:54:51 -0400

    update gitignore

--- lib/info.tsx
+++ lib/info.tsx
@@ -2,9 +2,33 @@ import me from "../app/placeholder.jpg";
 
 export const name = "Saad Selim";
 export const avatar = me;
-export const about = () => {
-  return <>Hey, I'm a developer.</>;
-};
-export const bio = () => {
-  return <>When I'm not coding, I'm probably playing video games.</>;
-};
+
+export const profession =
+  "I'm a full-stack developer specializing in React and Node.js.";
+
+export const bio = "When I'm not coding, I'm probably playing video games.";
+
+export const socialMediaLinks = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/saadselim/",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/S-Selim",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/ss6942",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node.js",
+  "HTML/CSS",
+  "Git",
+  "MongoDB",
+  "Express",
+];
